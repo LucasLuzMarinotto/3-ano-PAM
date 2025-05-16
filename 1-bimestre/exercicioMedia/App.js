97f4a33eb5ca658cd20ab363d63339b995d7389a
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2025-05-16 07:51:21 -0300

    Update App.js

--- 1-bimestre/exercicioMedia/App.js
+++ 1-bimestre/exercicioMedia/App.js
@@ -1,130 +1,68 @@
-import React,{useState} from 'react';
-import {Text, View, TextInput, Button, StyleSheet} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import {Box, Flex, Stack, Wrap, Selector} from "react-native-flex-layout";
 
-export default function App(){
-
-  const [n1, setN1] = useState()
-  const [n2, setN2] = useState()
-  const [n3, setN3] = useState()
-  const [n4, setN4] = useState()
-  const [aprovar, setAprovar] = useState()
-  const [resultado, setResultado] = useState()
-
-  const media = ()=>{
-    const r = (parseInt(n1) + parseInt(n2) + parseFloat(n3) + parseFloat(n4)) / 4
-    setResultado(r)
-
-    if(resultado < 6){
-      setAprovar('Reprovado')
-    }else{
-      setAprovar('Aprovado')
-    }
-  }
-
+export default function App() {
   return (
     <View style={styles.tudo}>
 
-      <View>
-        <Text style={styles.titulo}> Somando dois valores</Text>
-      </View>
+      <View Style={styles.header}>
+          <Text style={styles.textoGrande}>Outback Steakhouse</Text>
 
-      <View style={{margin:15}}>
-        <Text>Primeiro número:</Text>
-        <TextInput 
-          style={styles.caixaDeTexto}
-          placeholder='Digite aqui o primeiro número'
-          placeholderTextColor='#0ff'
-          autoFocus={true}
-          keyboardType={'numeric'}
-
-          onChangeText ={text => setN1(text)}
-        />
-      </View>
-
-      <View style={{margin:15}}>
-        <Text>Segundo número:</Text>
-        <TextInput 
-          style={styles.caixaDeTexto}
-          placeholder='Digite aqui o segundo número'
-          placeholderTextColor='#0ff'
-          keyboardType={'numeric'}
-
-          onChangeText ={text => setN2(text)}
-        />
-      </View>
-
-      <View style={{margin:15}}>
-        <Text>Segundo número:</Text>
-        <TextInput 
-          style={styles.caixaDeTexto}
-          placeholder='Digite aqui o terceiro número'
-          placeholderTextColor='#0ff'
-          keyboardType={'numeric'}
-
-          onChangeText ={text => setN3(text)}
-        />
-      </View>
-
-      <View style={{margin:15}}>
-        <Text>Segundo número:</Text>
-        <TextInput 
-          style={styles.caixaDeTexto}
-          placeholder='Digite aqui o quarto número'
-          placeholderTextColor='#0ff'
-          keyboardType={'numeric'}
-
-          onChangeText ={text => setN4(text)}
-        />
-      </View>
-
-      <View style={{margin:15}}>
-        <Button
-          title='Calcular a média'
-
-          onPress= {()=>media()}
-        />
-      </View>
-      
-      <View>
-        <Text style={styles.resposta}> Total da média: {resultado}</Text>
+          <Text style={styles.textoPequeno}>Nossos Produtos</Text>
+          <Text style={styles.textoPequeno}>Restaurantes</Text>
+          <Text style={styles.textoPequeno}>Espera Digital</Text>
+          <Text style={styles.textoPequeno}>Vale-Presente</Text>
+          <Text style={styles.textoPequeno}>Delivery</Text>
       </View>
 
       <View>
-        <Text style={styles.resposta}> Você foi: {aprovar}</Text>
+        <Text style={styles.textoGrande}>Explore o seu</Text>
+        <Text style={styles.textoGrande}>Momento Outback</Text>
+      </View>
+
+      <View>
+        <Text style={styles.textoPequeno}>Menu Principal</Text>
+        <Text style={styles.textoPequeno}>Lunch Menu</Text>
+        <Text style={styles.textoPequeno}>Billabong hour</Text>
+        <Text style={styles.textoPequeno}>Kids menu</Text>
+        <Text style={styles.textoPequeno}>Bebidas</Text>
+        <Text style={styles.textoPequeno}>Sobremesas</Text>
       </View>
 
     </View>
-  );
-}
+)};
 
 const styles = StyleSheet.create({
+
   tudo: {
-    backgroundColor:'#000',
+    backgroundColor:'#fff',
+    flex:1,
     height: '100%',
     width:'100%'
   },
 
-  titulo: {
-    color:'#00f',
-    fontSize:25,
+  header:{
+    backgroundColor:'#ff6400',
+    color:'#ff6400',
+    flex:1,
+    flexDirection: 'row',
+    width:'100%',
+    height:'20%'
+  },
+
+  textoPequeno:{
+    color:'#000000',
+    fontSize:15,
     textAlign:'center',
-    marginTop:25,
-    marginBottom:10
+    marginTop:30
   },
 
-  caixaDeTexto:{
-    borderWidth:1,
-    borderColor:'#0ff',
-    color:'#0ff',
-    borderRadius:40,
-    paddingLeft:25,
-    height:60
-  },
-
-  resposta:{
-    color:'#fff',
-    fontSize:25,
+  textoGrande:{
+    color:'#000000',
+    fontSize:30,
     textAlign:'center',
     marginTop:30
   }
+
 });
