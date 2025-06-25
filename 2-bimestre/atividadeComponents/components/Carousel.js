011945cb1013faae6b76a3520c2602b483707f6e
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2025-06-25 18:26:16 -0300

    Update Carousel.js

--- 2-bimestre/atividadeComponents/components/Carousel.js
+++ 2-bimestre/atividadeComponents/components/Carousel.js
@@ -1,1 +1,55 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
+export default function Cardapio() {
+  return (
+    <View>
+        <View>
+            <Text>Explore o seu</Text>
+            <Text>Momento Outback</Text>
+        </View>
+
+        <View>
+            <Text>Menu Principal</Text>
+            <Text>Lunch Menu</Text>
+            <Text>Billabong hour</Text>
+            <Text>Kids menu</Text>
+            <Text>Bebidas</Text>
+            <Text>Sobremesas</Text>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerRow: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  tudo: {
+    backgroundColor:'#fff',
+  },
+
+  textoPequeno:{
+    color:'#000',
+    fontSize:15,
+    textAlign:'center',
+    marginTop:30
+  },
+
+  textoGrande:{
+    color:'#000',
+    fontSize:30,
+    textAlign:'center',
+    marginTop:30
+  }
+});
