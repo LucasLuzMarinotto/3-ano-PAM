971fdc99dde9ef02b9c27dd71df8c6450a3ac9c8
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2025-06-25 19:04:39 -0300

    Update SliderData.tsx

--- 2-bimestre/atividadeComponents/data/SliderData.tsx
+++ 2-bimestre/atividadeComponents/data/SliderData.tsx
@@ -1,7 +1,14 @@
 import{ ImageSourcePropsType } from "react-native";
 
-export type ImageSliderType = {
-  title: string;
-  image: ImageSourcePropsType;
-  description: string;
+type ImageSliderType = {
+    title: string;
+    image: ImageSourcePropsType;
+    description: string;
 };
+
+const ImageSlider = [
+    {
+      title: 'imagem1',
+      image: require(@/src/imagens/)
+    }
+]
