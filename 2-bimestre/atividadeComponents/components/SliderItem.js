c6be933066e2f96504da8ab5e2056a7b5caa6170
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2025-06-25 19:36:11 -0300

    Update SliderItem.js

--- 2-bimestre/atividadeComponents/components/SliderItem.js
+++ 2-bimestre/atividadeComponents/components/SliderItem.js
@@ -4,5 +4,18 @@ import { StyleSheet, Text, View} from 'react-native'
 import React from 'react'
 
 type Props = {
-  item:
+  item: ImageSliderType;
+  index: number;
 }
+
+const SliderItem = ({item, index}: Props) =>{
+  return(
+    <View>
+      <Image source={item.image} style={{width: 300, height: 500}}/>
+      <Text>{item.title}</Text>
+    </View>
+}
+
+export default SlideItem
+
+const style = StyleSheet.create({})
